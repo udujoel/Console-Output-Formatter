53aa33b81044f3eb0a880b99baf58d25b6744f3f
Joell <amamihechi.udu@decagon.dev>
2020-02-10 18:26:40 +0100

    console display formatter

---
+++ Console Output Formatter/Formatter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Console_Output_Formatter
+{
+    public class Formatter
+    {
+        public static void ColoredMessage(string msg, ConsoleColor color = ConsoleColor.DarkMagenta)
+        {
+
+
+            CenterOutput(msg);
+            Console.ForegroundColor = color;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+            CenterOutput();
+        }
+
+        public static void CenterOutput(string textToDisplay = "")
+        {
+            Console.SetCursorPosition((Console.WindowWidth - textToDisplay.Length) / 2, Console.CursorTop);
+        }
+    }
+}
